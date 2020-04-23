467174c1c16528d74755708345bc8e1f112a0198
Mr. Noll <nollhyury@gmail.com>
2020-04-22 21:04:37 -0300

    upload of images

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,14 +1,15 @@
 import {Router} from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserSevice';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
-/**
- * Repositories
- * Services
- */
-
-usersRouter.post('/', async ( request, response) => {
+usersRouter.post('/', async ( request, response ) => {
   try {
     const {name, email, password} = request.body;
 
@@ -23,5 +24,20 @@ usersRouter.post('/', async ( request, response) => {
    return response.status(400).json({error: err.message})
  }
 });
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar') ,async ( request, response )=>{
+  try {
+    const updataUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updataUserAvatar.execute({
+      user_id : request.user.id,
+      avatarFileName: request.file.filename,
+    });
+
+    return response.json(user)
+  } catch (err) {
+    return response.status(400).json({error: err.message})
+  }
+})
 
 export default usersRouter;
