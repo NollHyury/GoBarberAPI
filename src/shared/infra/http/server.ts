7c108bf4cc50747dd2020f267cce54765d4708ea
Mr. Noll <nollhyury@gmail.com>
2020-06-20 15:23:50 -0300

    nova arquitetura

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,17 +1,18 @@
 import 'reflect-metadata'
+import cors from 'cors';
 
 import express, { Response, Request, NextFunction } from 'express';
 import 'express-async-errors';
 
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import uploadConfig from '../../../config/upload';
+import AppError from '../../errors/AppError';
 
-import './database';
+import '../typeorm';
 
 const app = express();
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory))
 app.use(routes);
